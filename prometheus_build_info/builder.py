467b14214e8dde10a64cbb0ffcf89cd63740bb45
Niels Albers <niels.albers@ordina.nl>
2020-08-21 15:24:42 +0200

    Added command line make-build-info

---
+++ prometheus_build_info/builder.py
@@ -0,0 +1,18 @@
+import click
+import json
+
+@click.command()
+@click.argument('appname')
+@click.argument('branch')
+@click.argument('revision')
+@click.argument('version')
+def make_build_info(appname, branch, revision, version):
+    buildinfo= {
+        "appname": appname,
+        "branch": branch,
+        "revision": revision,
+        "version": version
+    }
+    with open('buildinfo.json', 'w') as buildinfo_file:
+        buildinfo_file.write(json.dumps(buildinfo, indent=4, sort_keys=True))
+    click.echo("BuildInfo updated")
